4cba5bd00e79c360770df1e4cec4cd65f0a2b553
Bluenix <gabriel.nordholm@gmail.com>
2020-07-10 21:40:17 +0200

    Improve Brick documentation

--- scripts/Brick.cs
+++ scripts/Brick.cs
@@ -1,11 +1,11 @@
 using Godot;
 using System;
 
-/// <summary>
+/// <summary> The most basic brick. Deleted immediately on collision.
 public class Brick : StaticBody2D, IBrick
 {
     /// <summary>
-    /// Hit function for signalling when the object has collided and thus hit by the ball.
+    /// Hit function for signalling when the ball has collided with the brick, instantly deleting it.
     /// </summary>
     public void Hit(Ball ball)
     {
